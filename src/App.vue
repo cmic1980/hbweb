f5ca5123d53d2c6ed779cab374b6359f568580a8
Yu.miao <yu.miao@hinacom.com>
2018-05-14 23:43:18 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :span="24">
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+    <el-row style="margin-top: 10px">
+      <el-col :span="24">
+        <el-button @click="submit()">提交</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    }, created() {
+      this.load();
+    }, mounted() {
+
+    },
+    methods: {
+      async load() {
+
+        const response = await fetch('/api/analysis/list', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({})
+        });
+        const bodyText = await response.text();
+        const analysisResultList = JSON.parse(bodyText);
+      },
+      submit() {
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+        });
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
