72a335d943f7e34387cab7045e50ccb0f241c8e0
Yu.miao <yu.miao@hinacom.com>
2018-05-16 23:31:51 +0800

    绑定列表

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
       <el-col :span="24">
         <el-table
           :data="analysisResultList">
-          <el-table-column label="" width="30">
+          <el-table-column label="" width="130">
             <template slot-scope="scope">
               <!-- `checked` 为 true 或 false -->
-              <el-checkbox v-model="checked"></el-checkbox>
+              <el-button @click="setOrder(scope.row)">下单</el-button>
             </template>
           </el-table-column>
           <el-table-column
@@ -57,11 +57,28 @@
         </el-table>
       </el-col>
     </el-row>
-    <el-row style="margin-top: 10px">
-      <el-col :span="24">
-        <el-button @click="submit()">提交</el-button>
-      </el-col>
-    </el-row>
+
+    <el-dialog title="订单信息" :visible.sync="dialogFormVisible" width="30%">
+      <el-form :model="formOrder">
+        <el-form-item label="Symbol:">
+          <el-input v-model="formOrder.symbol" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="T:">
+          <el-input v-model="formOrder.t" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="Buy Time:">
+          <el-input v-model="formOrder.buyTime" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="Amount:">
+          <el-input v-model="formOrder.amount" ></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveOrder()">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -69,7 +86,9 @@
   export default {
     data() {
       return {
-        analysisResultList:[]
+        analysisResultList: [],
+        dialogFormVisible: false,
+        formOrder: {}
       }
     }, created() {
       this.load();
@@ -88,11 +107,38 @@
         });
         const bodyText = await response.text();
         this.analysisResultList = JSON.parse(bodyText);
-      },
-      submit() {
-        this.$alert('这是一段内容', '标题名称', {
-          confirmButtonText: '确定',
+
+
+      }, setOrder(analysisResult) {
+        let nowXDate = new XDate();
+        let year = nowXDate.getFullYear();
+        let month = nowXDate.getMonth();
+        let date = nowXDate.getDate();
+        let hour = analysisResult.sHour;
+
+        let buyTimeXDate = new XDate(year, month, date, hour, 0, 0, 0);
+        if (buyTimeXDate.valueOf() < nowXDate.valueOf()) {
+          buyTimeXDate = buyTimeXDate.addDays(1);
+        }
+
+        this.formOrder = {
+          symbol: analysisResult.symbol,
+          t: analysisResult.t,
+          buyTime: buyTimeXDate.toDate()
+        };
+        this.dialogFormVisible = true;
+      },async saveOrder(){
+        const response = await fetch('/api/order/add', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(this.formOrder)
         });
+        const bodyText = await response.text();
+
+        this.formOrder = {};
+        this.dialogFormVisible = false;
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -9,9 +9,8 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
-
-
 Vue.use(ElementUI);
+window.XDate = require('xdate')
 
 /* eslint-disable no-new */
 new Vue({
